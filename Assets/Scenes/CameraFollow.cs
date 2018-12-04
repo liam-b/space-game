56f6f752e01cc126bac5effedaa1edf36a0c9265
Liam Brennan <liam@anywhich.com>
2018-12-04 23:31:30 +1100

    made camera good

--- Assets/Scenes/CameraFollow.cs
+++ Assets/Scenes/CameraFollow.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 	public GameObject followObject;
+	private Rigidbody2D followRigidbody;
 	public new Camera camera;
 	public int maxZoom;
+
+	public GameObject planet;
+
+	private void Start() {
+		followRigidbody = followObject.GetComponent<Rigidbody2D>();
+	}
 	
 	void Update () {
 		float newZoom = camera.orthographicSize + Input.GetAxis("Mouse ScrollWheel") * camera.orthographicSize * 0.1f;
@@ -15,6 +22,13 @@ public class CameraFollow : MonoBehaviour {
 			camera.orthographicSize = maxZoom;
 		}
 
-  	transform.position = new Vector3(followObject.transform.position.x, followObject.transform.position.y, -1);
+		// Debug.Log(followRigidbody.centerOfMass);
+
+  	transform.position = new Vector3(followRigidbody.worldCenterOfMass.x, followRigidbody.worldCenterOfMass.y, -1);
+
+		Vector2 relative = transform.position - planet.transform.position;
+		transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2(relative.y, relative.x) * Mathf.Rad2Deg - 90);
+		
+  	// transform.position = new Vector3(followObject.transform.position.x, followObject.transform.position.y, -1);
 	}
 }
